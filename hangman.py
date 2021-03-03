d7826c2c60217f3c4ed1266077daaa653e0ad211
武田奈桜 <azukinako@takedanasakuranoMacBook-Pro.local>
2021-03-03 12:23:20 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,47 @@
+#coding:utf-8
+
+import random
+
+animals = ["cat","dog","pig","monky","sheep","camael","hamster","bird"]
+x = random.choice(animals)
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "_______        ",
+              "|              ",
+              "|     |        ",
+              "|     0        ",
+              "|    /|\       ",
+              "|    / \       ",
+              "|              "
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け！正解は {}.".format(word))
+
+
+hangman(x)
